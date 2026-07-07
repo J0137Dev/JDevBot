20465971950d57d10aa5b25bdaa07a4205d340ab
J-0137Dev <j0137martinez@gmail.com>
2026-07-06 21:10:01 -0400

    Change bot activity type from Playing to Streaming

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "J0137Dev bot community",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 4,
       },
     ],
   },
